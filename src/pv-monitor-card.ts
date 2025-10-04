111baf36e03af935fa7b3131977f76f3deb242e2
Markus Anzböck <hello@sjero.com>
2025-10-04 11:04:58 +0200

    0.0.3 fixed ha-card border

--- src/pv-monitor-card.ts
+++ src/pv-monitor-card.ts
@@ -108,8 +108,12 @@ export class PVMonitorCard extends LitElement {
             color: white;
             position: relative;
             overflow: hidden;
-            cursor: pointer;
-            box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.1);
+            backdrop-filter: blur(10px);
+            -webkit-backdrop-filter: blur(10px);
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            box-shadow:
+                    0 8px 32px 0 rgba(0, 0, 0, 0.37),
+                    inset 0 1px 0 0 rgba(255, 255, 255, 0.1);
         }
         .card::after {
             content: "";
